7a19a6ecb2ea75c31e0006428feb0690fe929cff
alexpts <alexpts@yandex.ru>
2022-12-24 11:26:55 +0300

    add vite-plugin-compression fot create gz single file

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVue3Resolver } from 'unplugin-vue-components/resolvers'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import viteCompression from 'vite-plugin-compression';
 
+// @todo https://github.com/vbenjs/vite-plugin-compression for single html mode
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,6 +14,7 @@ export default defineConfig({
   plugins: [
       vue(),
       viteSingleFile(),
+      viteCompression(),
       // https://cdmoro.github.io/bootstrap-vue-3/getting-started/#installation-vue-js
       Components({
           resolvers: [BootstrapVue3Resolver()]
